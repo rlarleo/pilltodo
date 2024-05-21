3645cc006d1c756c8d85cc5eccc5377816dd83b8
Kidae-Kim <kidae.kim@daydreamlab.co.kr>
2024-05-21 16:56:57 +0900

    firestore deviceId insert 로직 생성

--- pilltodo/lib/main.dart
+++ pilltodo/lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -21,9 +25,56 @@ class App extends StatelessWidget {
           titleTextStyle: TextStyle(color: Colors.white),
         ),
         body: Center(
-          child: Text('Hello world!'),
+          child: ElevatedButton(
+            onPressed: () {
+              _insertData();
+            },
+            child: Text('Insert Data to Firestore'),
+          ),
         ),
       ),
     );
+  }
+
+  Future<void> _insertData() async {
+    try {
+      // Firestore 인스턴스 가져오기
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+      // 데이터 추가
+      await firestore.collection('user').add({
+        'deviceId': await getDeviceUniqueId(),
+        'timestamp': DateTime.now(),
+      });
+
+      print('Data inserted successfully!');
+    } catch (e) {
+      print('Error inserting data: $e');
+    }
+  }
+
+  Future<String> getDeviceUniqueId() async {
+    var deviceIdentifier = 'unknown';
+
+    var deviceInfo = DeviceInfoPlugin();
+
+    if (kIsWeb) {
+      var webInfo = await deviceInfo.webBrowserInfo;
+      deviceIdentifier = webInfo.vendor! +
+          webInfo.userAgent! +
+          webInfo.hardwareConcurrency.toString();
+    } else if (Platform.isAndroid) {
+      var androidInfo = await deviceInfo.androidInfo;
+      // 불안..
+      deviceIdentifier = androidInfo.id;
+    } else if (Platform.isIOS) {
+      var iosInfo = await deviceInfo.iosInfo;
+      deviceIdentifier = iosInfo.identifierForVendor!;
+    } else if (Platform.isLinux) {
+      var linuxInfo = await deviceInfo.linuxInfo;
+      deviceIdentifier = linuxInfo.machineId!;
+    }
+
+    return deviceIdentifier;
   }
 }
