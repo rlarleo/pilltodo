2655e6b7baa0f652e8d9ca22751239646a98caff
Kidae-Kim <kidae.kim@daydreamlab.co.kr>
2024-05-23 12:14:54 +0900

    custom icon 생성, 하단바 생성

--- pilltodo/lib/main.dart
+++ pilltodo/lib/main.dart
@@ -3,220 +3,214 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:pilltodo/icons/custom_icons.dart';
 import 'package:pilltodo/widgets/button.dart';
+import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const MyApp());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFF181818),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 80,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          'Hey, Selena',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28,
-                            fontWeight: FontWeight.w800,
-                          ),
-                        ),
-                        Text(
-                          'Welcome back',
-                          style: TextStyle(
-                            color: Color.fromRGBO(255, 255, 255, 0.8),
-                            fontSize: 18,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 120,
-                ),
-                Text(
-                  'Total Balance',
-                  style: TextStyle(
-                    fontSize: 22,
-                    color: Colors.white.withOpacity(0.8),
+      title: 'Animated Notch Bottom Bar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  /// Controller to handle PageView and also handles initial page
+  final _pageController = PageController(initialPage: 0);
+
+  /// Controller to handle bottom nav bar and also handles initial page
+  final NotchBottomBarController _controller =
+      NotchBottomBarController(index: 0);
+
+  int maxCount = 5;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    /// widget list
+    final List<Widget> bottomBarPages = [
+      Page1(
+        controller: (_controller),
+      ),
+      const Page2(),
+      const Page3(),
+    ];
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: List.generate(
+            bottomBarPages.length, (index) => bottomBarPages[index]),
+      ),
+      extendBody: true,
+      bottomNavigationBar: (bottomBarPages.length <= maxCount)
+          ? AnimatedNotchBottomBar(
+              /// Provide NotchBottomBarController
+              notchBottomBarController: _controller,
+              color: Colors.white,
+              showLabel: true,
+              textOverflow: TextOverflow.visible,
+              maxLine: 1,
+              shadowElevation: 5,
+              kBottomRadius: 28.0,
+
+              /// restart app if you change removeMargins
+              removeMargins: false,
+              bottomBarWidth: 500,
+              showShadow: false,
+              durationInMilliSeconds: 300,
+
+              itemLabelStyle: const TextStyle(fontSize: 10),
+
+              elevation: 1,
+              bottomBarItems: const [
+                BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.home_filled,
+                    color: Colors.blueGrey,
+                  ),
+                  activeItem: Icon(
+                    Icons.home_filled,
+                    color: Color.fromARGB(255, 144, 251, 176),
                   ),
                 ),
-                const SizedBox(
-                  height: 5,
-                ),
-                const Text(
-                  '\$5 194 482',
-                  style: TextStyle(
-                    fontSize: 48,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
+                BottomBarItem(
+                  inActiveItem:
+                      Icon(Custom_Icons.pills, color: Colors.blueGrey),
+                  activeItem: Icon(
+                    Custom_Icons.pills,
+                    color: Colors.brown,
                   ),
                 ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Button(
-                      text: 'Transfer',
-                      bgColor: Color(0xFFF1B33B),
-                      textColor: Colors.black,
-                    ),
-                    Button(
-                      text: 'Request',
-                      bgColor: Color(0xFF1F2123),
-                      textColor: Colors.white,
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 100,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Wallets',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 36,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      'View All',
-                      style: TextStyle(
-                        color: Colors.white.withOpacity(0.8),
-                        fontSize: 18,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF1F2123),
-                    borderRadius: BorderRadius.circular(25),
+                BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.settings,
+                    color: Colors.blueGrey,
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(30),
-                    child: Row(
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              'Euro',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 32,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Row(
-                              children: [
-                                const Text(
-                                  '6 428',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  width: 5,
-                                ),
-                                Text(
-                                  'EUR',
-                                  style: TextStyle(
-                                    color: Colors.white.withOpacity(0.8),
-                                    fontSize: 20,
-                                  ),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
+                  activeItem: Icon(
+                    Icons.settings,
+                    color: Colors.grey,
                   ),
                 ),
               ],
-            ),
-          ),
+              onTap: (index) {
+                // log('current selected index $index');
+                _pageController.jumpToPage(index);
+              },
+              kIconSize: 24.0,
+            )
+          : null,
+    );
+  }
+}
+
+/// add controller to check weather index through change or not. in page 1
+class Page1 extends StatelessWidget {
+  final NotchBottomBarController? controller;
+
+  const Page1({Key? key, this.controller}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow,
+      child: Center(
+        /// adding GestureDetector
+        child: GestureDetector(
+          behavior: HitTestBehavior.translucent,
+          onTap: () {
+            controller?.jumpTo(2);
+          },
+          child: const Text('Page 1'),
         ),
       ),
     );
   }
+}
 
-  Future<void> _insertData() async {
-    try {
-      // Firestore 인스턴스 가져오기
-      FirebaseFirestore firestore = FirebaseFirestore.instance;
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
 
-      // 데이터 추가
-      await firestore.collection('user').add({
-        'deviceId': await getDeviceUniqueId(),
-        'timestamp': DateTime.now(),
-      });
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.green, child: const Center(child: Text('Page 2')));
+  }
+}
 
-      print('Data inserted successfully!');
-    } catch (e) {
-      print('Error inserting data: $e');
-    }
+class Page3 extends StatelessWidget {
+  const Page3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.red, child: const Center(child: Text('Page 3')));
+  }
+}
+
+Future<void> _insertData() async {
+  try {
+    // Firestore 인스턴스 가져오기
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+    // 데이터 추가
+    await firestore.collection('user').add({
+      'deviceId': await getDeviceUniqueId(),
+      'timestamp': DateTime.now(),
+    });
+
+    print('Data inserted successfully!');
+  } catch (e) {
+    print('Error inserting data: $e');
+  }
+}
+
+Future<String> getDeviceUniqueId() async {
+  var deviceIdentifier = 'unknown';
+
+  var deviceInfo = DeviceInfoPlugin();
+
+  if (kIsWeb) {
+    var webInfo = await deviceInfo.webBrowserInfo;
+    deviceIdentifier = webInfo.vendor! +
+        webInfo.userAgent! +
+        webInfo.hardwareConcurrency.toString();
+  } else if (Platform.isAndroid) {
+    var androidInfo = await deviceInfo.androidInfo;
+    // 불안..
+    deviceIdentifier = androidInfo.id;
+  } else if (Platform.isIOS) {
+    var iosInfo = await deviceInfo.iosInfo;
+    deviceIdentifier = iosInfo.identifierForVendor!;
+  } else if (Platform.isLinux) {
+    var linuxInfo = await deviceInfo.linuxInfo;
+    deviceIdentifier = linuxInfo.machineId!;
   }
 
-  Future<String> getDeviceUniqueId() async {
-    var deviceIdentifier = 'unknown';
-
-    var deviceInfo = DeviceInfoPlugin();
-
-    if (kIsWeb) {
-      var webInfo = await deviceInfo.webBrowserInfo;
-      deviceIdentifier = webInfo.vendor! +
-          webInfo.userAgent! +
-          webInfo.hardwareConcurrency.toString();
-    } else if (Platform.isAndroid) {
-      var androidInfo = await deviceInfo.androidInfo;
-      // 불안..
-      deviceIdentifier = androidInfo.id;
-    } else if (Platform.isIOS) {
-      var iosInfo = await deviceInfo.iosInfo;
-      deviceIdentifier = iosInfo.identifierForVendor!;
-    } else if (Platform.isLinux) {
-      var linuxInfo = await deviceInfo.linuxInfo;
-      deviceIdentifier = linuxInfo.machineId!;
-    }
-
-    return deviceIdentifier;
-  }
+  return deviceIdentifier;
 }
